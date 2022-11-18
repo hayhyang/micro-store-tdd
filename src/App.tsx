7a42b50d75d84283529bc00486e8c89ecac10283
hay <hayhyang@gmail.com>
2022-11-18 19:27:33 +0900

    [#2]feat_micro_store

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import Cart from "components/Cart";
+import Products from "components/Propducts";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Shop</h1>
+      <Cart />
+      <Products />
+    </div>
+  );
 }
 
 export default App;
